5e607590c0e84d01bfd2ed3a9685bc813a2c5601
Nate Kirby <nate@genius.com>
2016-11-16 10:31:28 -0500

    Swift 3 support v1

--- Classes/CPImageViewerAnimationTransition.swift
+++ Classes/CPImageViewerAnimationTransition.swift
@@ -8,30 +8,30 @@
 
 import UIKit
 
-public class CPImageViewerAnimationTransition: NSObject, UIViewControllerAnimatedTransitioning {
+open class CPImageViewerAnimationTransition: NSObject, UIViewControllerAnimatedTransitioning {
 
     
     /// Be false when Push or Present, and true when Pop or Dismiss
-    public var isBack = false
+    open var isBack = false
     
-    public func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+    open func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return 0.3
     }
     
-    public func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+    open func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
         
-        let fromVC = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
-        let toVC = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
-        let containerView = transitionContext.containerView()
-        let style = transitionContext.presentationStyle()
-        let finalFrame = transitionContext.finalFrameForViewController(toVC)
+        let fromVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
+        let toVC = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
+        let containerView = transitionContext.containerView
+        let style = transitionContext.presentationStyle
+        let finalFrame = transitionContext.finalFrame(for: toVC)
         
         // Solving the error of location of image view after rotating device and returning to previous controller. See ImageViewerViewController.init()
         // The OverFullScreen style don't need add toVC.view
         // The style is None when ImageViewerViewController.viewerStyle is CPImageViewerStyle.Push
-        if style != .OverFullScreen && isBack {
+        if style != .overFullScreen && isBack {
             containerView.addSubview(toVC.view)
-            containerView.sendSubviewToBack(toVC.view)
+            containerView.sendSubview(toBack: toVC.view)
             
             if toVC.view.bounds.size != finalFrame.size {
                 toVC.view.frame = finalFrame
@@ -41,56 +41,56 @@ public class CPImageViewerAnimationTransition: NSObject, UIViewControllerAnimate
         }
         
         let backgroundView = UIView(frame: finalFrame)
-        backgroundView.backgroundColor = UIColor.blackColor()
+        backgroundView.backgroundColor = UIColor.black
         containerView.addSubview(backgroundView)
         
         let fromImageView = (fromVC as! CPImageControllerProtocol).animationImageView
         let toImageView = (toVC as! CPImageControllerProtocol).animationImageView
-        let fromFrame = fromImageView.convertRect(fromImageView.bounds, toView: containerView)
-        let toFrame = toImageView.convertRect(toImageView.bounds, toView: containerView)
+        let fromFrame = fromImageView?.convert((fromImageView?.bounds)!, to: containerView)
+        let toFrame = toImageView?.convert((toImageView?.bounds)!, to: containerView)
         
-        let newImageView = UIImageView(frame: fromFrame)
-        newImageView.image = fromImageView.image
-        newImageView.contentMode = .ScaleAspectFit
+        let newImageView = UIImageView(frame: fromFrame!)
+        newImageView.image = fromImageView?.image
+        newImageView.contentMode = .scaleAspectFit
         containerView.addSubview(newImageView)
         
         if !isBack {
             backgroundView.alpha = 0.0
-            fromImageView.alpha = 0.0
+            fromImageView?.alpha = 0.0
         } else {
             backgroundView.alpha = 1.0
             fromVC.view.alpha = 0.0
         }
         
-        let duration = transitionDuration(transitionContext)
-        UIView.animateWithDuration(duration, delay: 0, options: .CurveEaseInOut, animations: {
+        let duration = transitionDuration(using: transitionContext)
+        UIView.animate(withDuration: duration, delay: 0, options: UIViewAnimationOptions(), animations: {
             if !self.isBack {
                 newImageView.frame = finalFrame
                 backgroundView.alpha = 1.0
             } else {
-                newImageView.frame = toFrame
+                newImageView.frame = toFrame!
                 backgroundView.alpha = 0.0
             }
             }, completion: { finished in
                 newImageView.removeFromSuperview()
                 backgroundView.removeFromSuperview()
                 
-                let cancel = transitionContext.transitionWasCancelled()
+                let cancel = transitionContext.transitionWasCancelled
                 
                 if !self.isBack {
                     if cancel {
-                        fromImageView.alpha = 1.0
+                        fromImageView?.alpha = 1.0
                     } else {
                         containerView.addSubview(toVC.view)
                     }
                 } else {
                     if cancel {
                         fromVC.view.alpha = 1.0
-                        if style != .OverFullScreen {
+                        if style != .overFullScreen {
                             toVC.view.removeFromSuperview()
                         }
                     } else {
-                        toImageView.alpha = 1.0
+                        toImageView?.alpha = 1.0
                     }
                 }
                 transitionContext.completeTransition(!cancel)
